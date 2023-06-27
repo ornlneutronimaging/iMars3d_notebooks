c3497c4e63d16eaa9cbb1d9dc8c8eb059ebd5ed5
JeanBilheux <bilheuxjm@ornl.gov>
2023-06-27 15:32:53 -0400

    Updated system after fixing bugs in imaging notebook repo. this refs #15

--- __code/config.py
+++ __code/config.py
@@ -2,6 +2,9 @@ debugging = True
 debugger_username = 'j35'
 debugger_folder = ['/Users/j35/HFIR/CG1D/',
                    '/Volumes/G-DRIVE/HFIR/CG1D/']
+debugger_instrument_folder = {'CG1D': "/Users/j35/HFIR/CG1D",
+                              'SNAP': "/Users/j35/SNS/SNAP",
+                              'VENUS': "/Users/j35/SNS/VENUS"}
 analysis_machine = 'cg1d-analysis2.ornl.gov'
 project_folder = 'IPTS_31148'
 percentage_of_images_to_use_for_roi_selection = 0.05
